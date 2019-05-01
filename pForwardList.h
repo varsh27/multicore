40fa3119f43db357a7c6c846638124e1a8337fbb
varsh27 <mvarsha2796@gmail.com>
2019-05-01 12:41:36 -0400

    changes

--- pForwardList.h
+++ pForwardList.h
@@ -146,14 +146,11 @@ public:
         if(!it) return;
         prev = it;
         it = it->next;
-        index--;
+        // index--;
       }
 
-      #pragma omp critical {
-        omp_set_lock(&(prev->nodeLock));
-        omp_set_lock(&(it->nodeLock));
-      }
-
+      omp_set_lock(&(prev->nodeLock));
+      omp_set_lock(&(it->nodeLock));
 
       pSListNode<T>* p = new pSListNode<T>(element);
       omp_init_lock(&(p->nodeLock));
@@ -178,7 +175,7 @@ public:
         if(!it) return;
         prev = it;
         it = it->next;
-        index--;
+        // index--;
       }
 
       if(!it || it->data == sentinalInt) return;
